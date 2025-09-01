603fef96e7fd664704826f7799647a283b7150d1
hia1394 <a4737465@naver.com>
2025-09-01 14:45:31 +0900

    fst

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import MyComponent from './components/MyComponent';
+import MyComponentFunc from './components/MyComponentFunc';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <MyComponent name="ReactJS" age={100}/>
 
-export default App
+      <hr/>
+      <MyComponentFunc name="함수형">
+        <p>함수형 컴포넌트의 하위 엘리먼트</p>
+      </MyComponentFunc>
+    </div>
+  );
+};
+
+export default App;
